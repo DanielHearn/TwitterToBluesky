eb72545b8b883ee240d531ed0271f18cec7eb9fe
Daniel Hearn <DanielHearn@users.noreply.github.com>
2024-12-10 22:59:02 +0000

    Fix following detection

--- scripts/detector.js
+++ scripts/detector.js
@@ -5,9 +5,7 @@ const checkAccount = (urlName, name, checkName) => {
     `https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile?actor=${nameUsed}.bsky.social`,
     (data) => {
       const jsonData = JSON.parse(data)
-      console.log(jsonData)
       if (jsonData.error === 'InvalidRequest') {
-        console.log('NO ACC: ', nameUsed)
         if (checkName) {
           checkAccount(urlName, name, false)
         }
@@ -18,8 +16,9 @@ const checkAccount = (urlName, name, checkName) => {
         }
         const label = document.createElement('div')
         label.id = 'TwitterToBlueSkyProfileLabel'
+        label.className = 'TwitterToBlueSkyProfile'
         const link = document.createElement('a')
-        link.innerText = 'BLUESKY ACCOUNT'
+        link.innerText = 'BlueSky Profile'
         link.href = `https://bsky.app/profile/${nameUsed}.bsky.social`
         label.appendChild(link)
         const target = document.querySelector('[data-testid="UserName"]')
@@ -29,20 +28,127 @@ const checkAccount = (urlName, name, checkName) => {
   )
 }
 
-window.navigation.addEventListener('navigate', (event) => {
-  console.log('location changed!')
-  let loaded = false
-  const id = setInterval(() => {
-    const urlName = window.location.pathname.split('/').pop().toLocaleLowerCase()
-    const target = document.querySelector('[data-testid="UserName"]')
-    if (target) {
-      const name = target.querySelector('span').innerText.toLocaleLowerCase()
-      console.log(urlName, name)
-      if (target) {
-        checkAccount(urlName, name, true)
-        loaded = true
-        clearInterval(id)
+function sendMessage(name) {
+  return new Promise((resolve) => {
+    chrome.runtime.sendMessage(
+      `https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile?actor=${name}.bsky.social`,
+      resolve,
+    )
+  })
+}
+
+const getAccount = async (urlName, name) => {
+  const data1 = await sendMessage(urlName)
+  const jsonData1 = JSON.parse(data1)
+  if (jsonData1.error === 'InvalidRequest') {
+    const data2 = await sendMessage(name)
+    const jsonData2 = JSON.parse(data2)
+    if (jsonData2.error === 'InvalidRequest') {
+      return false
+    } else {
+      return name
+    }
+  } else {
+    return urlName
+  }
+}
+
+const checkFollowingPage = async () => {
+  const listElements = document.querySelectorAll(
+    '[data-testid="primaryColumn"] > div > section > div > div > div',
+  )
+  console.log(listElements.length)
+  for (const element of listElements) {
+    const target = element.querySelector('span')
+    const profileLink = element.querySelector('a')?.href
+    if (
+      target &&
+      profileLink &&
+      !profileLink.includes('search') &&
+      !profileLink.includes('explore') &&
+      !element.querySelector('.TwitterToBlueSkyFollowingLabel') &&
+      !element.querySelector('.TwitterToBlueSkyNoAccount')
+    ) {
+      const linkName = profileLink.split('/')[0]
+      const name = element.querySelector('span').innerText
+      const account = await getAccount(linkName, name)
+      console.log(name)
+      if (account) {
+        const existingLabel = element.querySelector('.TwitterToBlueSkyFollowingLabel')
+        if (existingLabel) {
+          existingLabel.remove()
+        }
+        const label = document.createElement('div')
+        label.className = 'TwitterToBlueSkyFollowingLabel'
+        const link = document.createElement('a')
+        link.innerText = 'BlueSky Profile'
+        link.href = `https://bsky.app/profile/${account}.bsky.social`
+        label.appendChild(link)
+        target.appendChild(label)
+      } else {
+        const existingLabel = element.querySelector('.TwitterToBlueSkyFollowingLabel')
+        if (existingLabel) {
+          existingLabel.remove()
+        }
+        const label = document.createElement('div')
+        label.className = 'TwitterToBlueSkyNoAccount'
+        target.appendChild(label)
       }
     }
-  }, 500)
+  }
+}
+
+const checkProfilePage = () => {
+  const urlName = window.location.pathname.split('/')[1].toLocaleLowerCase()
+  const target = document.querySelector('[data-testid="UserName"]')
+  if (target) {
+    const name = target.querySelector('span').innerText.toLocaleLowerCase()
+    if (target) {
+      checkAccount(urlName, name, true)
+      loaded = true
+    }
+  }
+}
+
+const styles = document.createElement('style')
+styles.innerHTML = `
+[data-testid="UserName"] {
+  gap: 6px;
+}
+
+.TwitterToBlueSkyProfile, TwitterToBlueSkyFollowingLabel {
+  padding: 0.5em;
+  border-radius: 6px;
+  background: #0085ff;
+  font-family: TwitterChirp, -apple-system, BlinkMacSystemFont, "Segoe UI";
+  display: flex;
+  height: max-content;
+  a {
+    text-decoration: none;
+    color: white;
+    font-family: TwitterChirp, -apple-system, BlinkMacSystemFont, "Segoe UI";
+    &:visited {
+      color: white;
+      text-decoration: none;
+    }
+  }
+}`
+
+document.querySelector('head').appendChild(styles)
+let followingId = null
+
+window.navigation.addEventListener('navigate', (event) => {
+  let loaded = false
+  clearInterval(followingId)
+  const id = setTimeout(() => {
+    if (window.location.pathname.split('/')[2] === 'following') {
+      setInterval(() => {
+        if (window.location.pathname.split('/')[2] === 'following') {
+          checkFollowingPage()
+        }
+      }, 1000)
+    } else {
+      checkProfilePage()
+    }
+  }, 50)
 })
